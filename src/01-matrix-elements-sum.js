ae525db47fe7c9cec8532ce8d843f959c4689498
Vagiz Musin <vagiz_mus@mail.ru>
2021-04-11 19:07:47 +0600

    fix: code update

--- src/01-matrix-elements-sum.js
+++ src/01-matrix-elements-sum.js
@@ -18,13 +18,12 @@ function getMatrixElementsSum(/* matrix */) {
   const copyMatrix = matrix;
   for (let i = 0; i < copyMatrix.length - 1; i++) {
     for (let j = 0; j < copyMatrix[i].length; j++) {
-      console.log(copyMatrix[i][j]);
       if (copyMatrix[i][j] === 0) {
         copyMatrix[i + 1][j] = 0;
       }
     }
   }
-  return copyMatrix.flat(Infinity).reduce((acc, cur) => (acc += cur));
+  return copyMatrix.flat(Infinity).reduce((a, c) => (a + c));
 }
 
 module.exports = getMatrixElementsSum;
