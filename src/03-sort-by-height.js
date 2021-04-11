28611110ea08bcce19f1a6965c631e2256f58723
Vagiz Musin <vagiz_mus@mail.ru>
2021-04-11 23:59:27 +0600

    feat: add task solution

--- src/03-sort-by-height.js
+++ src/03-sort-by-height.js
@@ -1,16 +1,23 @@
-/**
- * Given an array with heights, sort them except if the value is -1.
- *
- * @param {Array} arr
- * @return {Array}
- *
- * @example
- * arr = [-1, 150, 190, 170, -1, -1, 160, 180]
- *
- * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
- */
-function sortByHeight(/* arr */) {
-  throw new Error('Not implemented');
+function sortByHeight(arr) {
+  let k = 0;
+  const output = [];
+  const middleArr = [];
+  arr.forEach((element) => {
+    if (element !== -1) {
+      middleArr.push(element);
+    }
+  });
+  middleArr.sort((a, b) => a - b);
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === -1) {
+      output.push(arr[i]);
+      k += 1;
+    } else {
+      output.push(middleArr[0]);
+      middleArr.shift();
+    }
+  }
+  return k > 0 ? output : arr.sort((a, b) => a - b);
 }
 
 module.exports = sortByHeight;
