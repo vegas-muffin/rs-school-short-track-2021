5471334926c3215bff12067423cb669d95cc794f
Vagiz Musin <vagiz_mus@mail.ru>
2021-04-13 22:14:01 +0600

    add task solution

--- src/05-delete-digit.js
+++ src/05-delete-digit.js
@@ -9,8 +9,19 @@
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new Error('Not implemented');
+function deleteDigit(n) {
+  const mainArr = [];
+  const copyN1 = n.toString();
+
+  for (let i = 0; i < copyN1.length; i++) {
+    mainArr.push(copyN1);
+  }
+
+  const resultArr = [];
+  for (let i = 0; i < mainArr.length; i++) {
+    resultArr.push(Number(mainArr[i].slice(0, i) + mainArr[i].slice(i + 1)));
+  }
+  return Math.max.apply(null, resultArr);
 }
 
 module.exports = deleteDigit;
